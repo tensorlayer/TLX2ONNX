0be7a9f5e2d09d958d42262f8a1a5f2d4c31052b
hanjr92 <jiaronghan@outlook.com>
2022-07-06 15:11:07 +0800

    update test_rnn.py

--- tests/test_rnn.py
+++ tests/test_rnn.py
@@ -7,26 +7,22 @@ os.environ["TL_BACKEND"] = 'torch'
 # os.environ["TL_BACKEND"] = 'mindspore'
 import tensorlayerx as tlx
 from tensorlayerx.nn import Module
-from tensorlayerx.nn import Linear, RNN, Embedding, LSTM
+from tensorlayerx.nn import  RNN, LSTM
 from tlx2onnx.main import export
 import onnxruntime as rt
 import numpy as np
 tlx.set_seed(42)
-prev_h = np.random.random([2, 2, 5]).astype(np.float32)
-prev_h = tlx.convert_to_tensor(prev_h)
-# prev_h = tlx.nn.Input(shape=[1, 24, 64])
 
 class ImdbNet(Module):
 
     def __init__(self):
         super(ImdbNet, self).__init__()
-        self.rnn = RNN(input_size=5, hidden_size=5, bidirectional=False, num_layers=4)
+        self.rnn = RNN(input_size=5, hidden_size=5, bidirectional=True, num_layers=4)
     def forward(self, x):
         x, _ = self.rnn(x)
         return x
 model = ImdbNet()
-inputs = prev_h
-input = tlx.nn.Input(shape=[4, 2, 5])
+input = tlx.nn.Input(shape=[2, 2, 5])
 model.set_eval()
 output = model(input)
 print("RNN tlx output", output)
