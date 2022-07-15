5e9dfb86ebe563ff9a36739f7eae5a9ba035e092
Eric_lai <laicheng_vip@163.com>
2022-07-15 15:10:37 +0800

    TLX2ONNX 0.0.1 release

---
+++ tests/test_layernorm.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+os.environ["TL_BACKEND"] = 'tensorflow'
+import tensorlayerx as tlx
+from tensorlayerx.nn import Module
+from tensorlayerx.nn import LayerNorm
+from tlx2onnx.main import export
+import onnxruntime as rt
+import numpy as np
+
+
+class NET(Module):
+    def __init__(self):
+        super(NET, self).__init__()
+        self.layernorm = LayerNorm([50, 50, 32])
+
+    def forward(self, x):
+        x = self.layernorm(x)
+        return x
+
+net = NET()
+print(type(net))
+net.set_eval()
+input = tlx.nn.Input(shape=(10, 50, 50, 32))
+onnx_model = export(net, input_spec=input, path='layernorm.onnx', enable_onnx_checker=False)
+print("tlx out", input)
+
+# Infer Model
+sess = rt.InferenceSession('layernorm.onnx')
+
+input_name = sess.get_inputs()[0].name
+output_name = sess.get_outputs()[0].name
+
+input_data = np.array(input, dtype=np.float32)
+
+result = sess.run([output_name], {input_name: input_data})
+print("onnx out", result)
--- tlx2onnx/op_mapper/nn/normalization.py
+++ tlx2onnx/op_mapper/nn/normalization.py
@@ -80,3 +80,62 @@ class BatchNorm():
                                      )
             onnx_node.append(bn_node)
         return onnx_node, onnx_value, onnx_init
+
+
+@OpMapper(['LayerNorm'])
+class LayerNorm():
+    # supports v17
+
+    @classmethod
+    def version_17(cls, node, **kwargs):
+        onnx_node = []
+        onnx_value = []
+        onnx_init = []
+
+        # input , output, data_format
+        x = node['in_nodes_name'][0]
+        x_shape = node['in_tensors'][0]
+
+        out_shape = node['out_tensors'][0]
+        out_v = helper.make_tensor_value_info(node['out_nodes_name'][0], NP_TYPE_TO_TENSOR_TYPE[node['dtype']],
+                                              shape=node['out_tensors'][0])
+        onnx_value.append(out_v)
+
+        spatial = 2
+        # get parameters
+        beta_name = node['node'].layer.name + '/beta'
+        beta_weight = numpy_helper.from_array(arr=to_numpy(node['node'].layer.beta), name=beta_name)
+        onnx_init.append(beta_weight)
+
+        gamma_name = node['node'].layer.name + '/gamma'
+        gamma_weight = numpy_helper.from_array(arr=to_numpy(node['node'].layer.gamma), name=gamma_name)
+        onnx_init.append(gamma_weight)
+
+        epsilon = node['node'].layer.epsilon
+
+        # if data_format == 'channels_last':
+            # channels last conver weights and input
+        x_shape = make_shape_channels_first(x_shape)
+        out_temp_shape = make_shape_channels_first(out_shape)
+        # make channels transpose
+        t_x = helper.make_tensor_value_info(node['in_nodes_name'][0] + 't',
+                                            NP_TYPE_TO_TENSOR_TYPE[node['dtype']], shape=x_shape)
+        onnx_value.append(t_x)
+        tx_node, x = make_node('Transpose', inputs=[x], outputs=[node['in_nodes_name'][0] + 't'],
+                               perm=get_channels_first_permutation(spatial))
+        onnx_node.append(tx_node)
+        # make batch normalization
+        out_temp = helper.make_tensor_value_info(node['out_nodes_name'][0] + 'bn',
+                                                 NP_TYPE_TO_TENSOR_TYPE[node['dtype']], shape=out_temp_shape)
+        onnx_value.append(out_temp)
+        ln_node, out = make_node('LayerNormalization',
+                                 inputs=[node['in_nodes_name'][0] + 't', beta_name, gamma_name],
+                                 outputs=[node['out_nodes_name'][0] + 'bn'], epsilon=epsilon
+                                 )
+        onnx_node.append(ln_node)
+        # make channels transpose
+        t_out = helper.make_tensor_value_info(node['out_nodes_name'][0], NP_TYPE_TO_TENSOR_TYPE[node['dtype']], shape=out_shape)
+        onnx_value.append(t_out)
+        tout_node, _ = make_node('Transpose', inputs=[out], outputs=node['out_nodes_name'], perm=get_channels_last_permutation(spatial))
+        onnx_node.append(tout_node)
+        return onnx_node, onnx_value, onnx_init
